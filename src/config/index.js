82841d2bac1a08df79d15a43ce17d1fea1d5c48a
gopoma <gordono@unsa.edu.pe>
2023-08-30 01:35:07 -0500

    prepared for deployment

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const config = {
+    mode: import.meta.env.VITE_MODE,
+    serverPath: import.meta.env.VITE_SERVER_PATH,
+};
--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,11 +1,12 @@
 import { createContext } from 'react';
 
 import { useSocket } from '../hooks';
+import { config } from '../config';
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-    const { socket, online } = useSocket('http://localhost:4000');
+    const { socket, online } = useSocket(config.serverPath);
 
     return (
         <SocketContext.Provider value={{ socket, online }}>
